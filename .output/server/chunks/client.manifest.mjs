a37ecca9cb0886764a84a13dbde43bd985899ff4
Beto Nogueira <beto@betonogueira.com.br>
2021-10-17 16:05:15 -0300

    Clean all nuxt

--- .output/server/chunks/client.manifest.mjs
+++
@@ -1,9 +0,0 @@
-const client_manifest = {
-  "node_modules/nuxt3/dist/app/entry.mjs": {
-    "file": "entry-1405c034.mjs",
-    "src": "node_modules/nuxt3/dist/app/entry.mjs",
-    "isEntry": true
-  }
-};
-
-export { client_manifest as default };
